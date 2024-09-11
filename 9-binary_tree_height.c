32feb9992bf597a3bdc2a597393169acc17c05d5
piusezekiel <e.pius1@alustudent.com>
2024-09-11 15:42:01 +0200

    9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,35 +1,37 @@
 #include "binary_trees.h"
 
-
 int max(int a, int b);
 
 /**
- * binary_tree_height - Measures the height of a binary tree
- * @tree: A pointer to the root node of the tree to measure the height
- *
- * Return: The height of the tree, or 0 if tree is NULL
- */
+* binary_tree_height - measures the height of a binary tree
+* @tree: pointer to the root node of the tree to measure
+* Return: height of the tree, or 0 if tree is NULL
+*/
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-size_t left_height, right_height;
+	/* if tree is NULL, return 0 */
+	if (tree == NULL)
+		return (0);
 
-if (tree == NULL)
-return (0);
+	/* if tree has no children, return 0 */
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
 
-left_height = binary_tree_height(tree->left);
-right_height = binary_tree_height(tree->right);
-
-return (1 + max(left_height, right_height));
+	/* if tree has children, return 1 + height of tallest subtree */
+	return (1 + max(binary_tree_height(tree->left),
+			binary_tree_height(tree->right)));
 }
 
 /**
- * max - Compares two integers
- * @a: first number
- * @b: second number
- * Return: The the larger of the two numbers
- */
-
+* max - returns the max of two numbers
+* @a: first number
+* @b: second number
+* Return: max of a and b
+*/
 int max(int a, int b)
 {
-return (a > b ? a : b);
+	if (a > b)
+		return (a);
+	else
+		return (b);
 }
