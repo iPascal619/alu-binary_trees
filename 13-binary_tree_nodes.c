4f1ce928a04009b6b0dbad4f4a4d600ee4ac0d09
piusezekiel <e.pius1@alustudent.com>
2024-09-11 16:13:23 +0200

    13-binary_tree_nodes.c

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -11,7 +11,10 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 if (tree == NULL)
 return (0);
 
-size_t count = (tree->left != NULL || tree->right != NULL) ? 1 : 0;
+size_t count = 0;
+
+if (tree->left != NULL || tree->right != NULL)
+count = 1;
 
 return (count + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
 }
