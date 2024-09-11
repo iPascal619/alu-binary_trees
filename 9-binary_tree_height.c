807c22aa463df11bc6f32d20299dbf832f9fd689
piusezekiel <e.pius1@alustudent.com>
2024-09-11 15:10:59 +0200

    9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -15,8 +15,5 @@ return (0);
 left_height = binary_tree_height(tree->left);
 right_height = binary_tree_height(tree->right);
 
-if (left_height > right_height)
-return (left_height + 1);
-else
-return (right_height + 1);
+return (1 + (left_height > right_height ? left_height : right_height));
 }
